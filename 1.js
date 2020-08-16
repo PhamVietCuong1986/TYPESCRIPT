c46a8df2e6ebe6d85da99a656ff7d6942e10892d
Nguyen van D <vietcuong1226@gmail.com>
2020-08-16 16:54:06 +0700

    khai bao bien trong type script

---
+++ 1.js
@@ -0,0 +1,6 @@
+var chu;
+chu = "viet";
+var chu2;
+chu2 = " sinh nam 1986";
+console.log("\nXin chao " + chu + ", co phai ban " + chu2 + "!\n");
+// long dau ra bang tu khoa ${} trong typescript
---
+++ 1.ts
@@ -0,0 +1,9 @@
+let chu: string;
+chu = "viet";
+
+var chu2: string;
+chu2 = " sinh nam 1986";
+console.log(`
+Xin chao ${chu}, co phai ban ${chu2}!
+`);
+// long dau ra bang tu khoa ${} trong typescript
