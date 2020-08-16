1d0267281ebc6e9657e3efdb1e0240d51f492f89
Nguyen van D <vietcuong1226@gmail.com>
2020-08-16 17:16:23 +0700

    khai bao bien mang number, mang string

--- 1.js
+++ 1.js
@@ -1,6 +1,21 @@
-var chu;
-chu = "viet";
-var chu2;
-chu2 = " sinh nam 1986";
-console.log("\nXin chao " + chu + ", co phai ban " + chu2 + "!\n");
+// let chu: string;
+// chu = "viet";
+// var chu2: string;
+// chu2 = " sinh nam 1986";
+// console.log(`
+// Xin chao ${chu}, co phai ban ${chu2}!
+// `);
 // long dau ra bang tu khoa ${} trong typescript
+// let x: number;
+// x = 10;
+// console.log(x);
+// var student : string[];
+// student = ["lam","an","hieu"];
+// for(var i = 0 ; i < student.length; i++){
+// console.log(student[i]);
+// }
+// mang kieu so
+var songNguyen = [2, 4, 6];
+for (var i = 0; i < songNguyen.length; i++) {
+    console.log(songNguyen[i]);
+}
--- 1.ts
+++ 1.ts
@@ -1,9 +1,25 @@
-let chu: string;
-chu = "viet";
+// let chu: string;
+// chu = "viet";
 
-var chu2: string;
-chu2 = " sinh nam 1986";
-console.log(`
-Xin chao ${chu}, co phai ban ${chu2}!
-`);
+// var chu2: string;
+// chu2 = " sinh nam 1986";
+// console.log(`
+// Xin chao ${chu}, co phai ban ${chu2}!
+// `);
 // long dau ra bang tu khoa ${} trong typescript
+
+// let x: number;
+// x = 10;
+// console.log(x);
+
+// var student : string[];
+// student = ["lam","an","hieu"];
+// for(var i = 0 ; i < student.length; i++){
+// console.log(student[i]);
+// }
+
+// mang kieu so
+ var songNguyen : number[] = [2,4,6];
+ for( var i =0; i < songNguyen.length; i++){
+     console.log(songNguyen[i]);
+ }
